27852aefe181165acfb08f26ad8af0847274e575
monicajaimesc <monikmaja@gmail.com>
2020-02-14 15:18:35 -0500

    task 1 almost completed

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,6 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
+import uuid
 
 
 # flask setup
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 port = 5000
 host = '0.0.0.0'
+
 
 
 # begin flask page rendering
@@ -34,13 +36,17 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
+    cache_id = str(uuid.uuid4())
     return render_template('100-hbnb.html',
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users)
+                           users=users,
+                           cache_id=cache_id)
 
 if __name__ == "__main__":
     """
     MAIN Flask App"""
     app.run(host=host, port=port)
+    
+    
