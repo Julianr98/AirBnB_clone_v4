e819cd12efbcedfbc1a2741a66739f273058828c
monicajaimesc <monikmaja@gmail.com>
2020-02-15 21:13:16 +0000

    delete, add and init function

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,29 @@
+let amenities = []
+function one_array(amenities) {
+        $('.amenities H4').text(amenities.join(', '))
+}
+
+function deleteThis(data, list) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] === data) {
+            list.splice(i, 1)
+        }
+    }
+}
+
+
+function init() {
+    $('.amenities input').on('change', function(){
+    let check_object = $(this)[0].checked
+        if (check_object === true){
+            amenities.push($(this).data('name'))
+        } else {
+            deleteThis($(this).data('name'), amenities)
+        }
+        one_array(amenities)
+    })
+
+
+}
+
+$(document).ready(init)
