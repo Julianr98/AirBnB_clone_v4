437058453280153bb88c740036fa93383d58ff08
monicajaimesc <monikmaja@gmail.com>
2020-02-18 13:39:42 +0000

    semistandard fixing

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,29 +1,26 @@
-let amenities = []
-function one_array(amenities) {
-        $('.amenities H4').text(amenities.join(', '))
+const amenities = [];
+function oneArray (amenities) {
+  $('.amenities H4').text(amenities.join(', '));
 }
 
-function deleteThis(data, list) {
-    for (let i = 0; i < list.length; i++) {
-        if (list[i] === data) {
-            list.splice(i, 1)
-        }
+function deleteThis (data, list) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === data) {
+      list.splice(i, 1);
     }
+  }
 }
 
-
-function init() {
-    $('.amenities input').on('change', function(){
-    let check_object = $(this)[0].checked
-        if (check_object === true){
-            amenities.push($(this).data('name'))
-        } else {
-            deleteThis($(this).data('name'), amenities)
-        }
-        one_array(amenities)
-    })
-
-
+function init () {
+  $('.amenities input').on('change', function () {
+    const checkObject = $(this)[0].checked;
+    if (checkObject === true) {
+      amenities.push($(this).data('name'));
+    } else {
+      deleteThis($(this).data('name'), amenities);
+    }
+    oneArray(amenities);
+  });
 }
 
-$(document).ready(init)
+$(document).ready(init);
